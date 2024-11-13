0e43c0657e9751f43e53d273dbd76b8cd46c5713
Simon Findling <simon.findling@outlook.de>
2024-11-13 15:30:44 +0100

    fix: revert markup escapement for interpolated text

--- src/Empowered.CommandLine.Extensions/Extensions/AnsiConsoleExtensions.cs
+++ src/Empowered.CommandLine.Extensions/Extensions/AnsiConsoleExtensions.cs
@@ -6,14 +6,14 @@ public static class AnsiConsoleExtensions
 {
 
     public static void Info(this IAnsiConsole console, string message) =>
-        console.MarkupLineInterpolated($"[white]{message}[/]");
+        console.MarkupLine($"[white]{message}[/]");
 
     public static void Success(this IAnsiConsole console, string message) =>
-        console.MarkupLineInterpolated($"[green]{message}[/]");
+        console.MarkupLine($"[green]{message}[/]");
 
     public static void Warning(this IAnsiConsole console, string message) =>
-        console.MarkupLineInterpolated($"[yellow]{message}[/]");
+        console.MarkupLine($"[yellow]{message}[/]");
 
     public static void Error(this IAnsiConsole console, string message) =>
-        console.MarkupLineInterpolated($"[red]{message}[/]");
+        console.MarkupLine($"[red]{message}[/]");
 }
